3d8678a91b69f7fd9fe41a03138aeb4f36f1a00e
habibzuhaer <123pxdfg7Pxx>
2019-09-12 21:11:27 +0200

    progress 11% create encoder  error logger

--- src/main/java/com/web_app/springbootapp/web/controllers/RegistrationController.java
+++ src/main/java/com/web_app/springbootapp/web/controllers/RegistrationController.java
@@ -3,25 +3,29 @@ package com.web_app.springbootapp.web.controllers;
 import com.web_app.springbootapp.model.entities.User;
 import com.web_app.springbootapp.model.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/register")
+@RequestMapping("/registration")
 public class RegistrationController {
 
     private UserRepository userRepository;
 
-    @Autowired
-    public RegistrationController(UserRepository userRepository) {
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired // Wstrzyknięcie przez konstruktor
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping
     public String preparepRegistrationPage() {
-        return "/WEB-INF/views/registration-page.jsp";
+        return "/WEB-INF/views/registration-form.jsp";
     }
 
     @PostMapping
